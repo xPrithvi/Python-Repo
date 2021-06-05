346b8d2d9f321114dc7c17dd05c4618d2d760998
Prithvi <34770840+xPrithvi@users.noreply.github.com>
2021-06-05 19:33:49 +0400

    Add files via upload

--- Matplotlib/PendulumGraphAnimation.py
+++ Matplotlib/PendulumGraphAnimation.py
@@ -3,18 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 plt.style.use('dark_background')
+plt.gcf().suptitle("The Ideal Pendulum", fontsize = 16)
 
 X_VALUES = []
 Y_VALUES = []
 
 INITIAL_THETA = 0.5
 INITIAL_TIME = 0
-TIME_STEP = 0.1
+GRAVITY = 9.81
+TIME_STEP = 1/60
 LENGTH = 1
 INDEX = 0
 
 def function_iteration(i):
-    global INITIAL_THETA, INITIAL_TIME, TIME_STEP, LENGTH, INDEX
+    global INITIAL_THETA, INITIAL_TIME, TIME_STEP, LENGTH, GRAVITY, INDEX
     X_VALUE = LENGTH*np.sin(INITIAL_THETA * np.cos(INITIAL_TIME + (INDEX * TIME_STEP)))
     Y_VALUE = LENGTH - (LENGTH*np.cos(INITIAL_THETA * np.cos(INITIAL_TIME + (INDEX * TIME_STEP))))
     INDEX += 1
@@ -24,8 +26,9 @@ def function_iteration(i):
     plt.ylim([-LENGTH, LENGTH])
     plt.plot([0, X_VALUE], [LENGTH, Y_VALUE], color = "white")
     plt.plot(0, LENGTH, "o", X_VALUE, Y_VALUE, "o", color = "white")
-    plt.xlabel("Ideal Pendulum")
+    plt.xlabel("$\Theta_{i} = " + str(INITIAL_THETA) + ",L = " + str(LENGTH) +
+                ",t = " + str(INITIAL_TIME + (INDEX * TIME_STEP))[0:3] + "$")
 
-ANIMATION = FuncAnimation(plt.gcf(), function_iteration, frames = 720,  interval = 0)
-ANIMATION.save("PendulumGraphAnimation.mp4", dpi = 300, fps = 60, writer = "ffmpeg")
+ANIMATION = FuncAnimation(plt.gcf(), function_iteration, frames = 180,  interval = 0)
+#ANIMATION.save("PendulumGraphAnimation.mp4", dpi = 300, fps = 60, writer = "ffmpeg")
 plt.show()
